ee56eca12a0a4ccf3da7f730717c5557cf2d8b8f
Jason Anton <rev.jt.anton@gmail.com>
2024-11-25 13:56:24 -0500

    feat: initializing the repo (#1)
    * feat: initializing the repo
    
    * ci: removing profile from provider which is not needed

---
+++ terraform/provider.tf
@@ -0,0 +1,28 @@
+terraform {
+  backend "s3" {
+    bucket         = "conversadocs-terraform"
+    key            = "state/prime/landing_zone.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-lock-table"
+    encrypt        = true
+  }
+
+  required_version = "1.9.8"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.76.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+
+  default_tags {
+    tags = {
+      Terraform = "true"
+    }
+  }
+}
